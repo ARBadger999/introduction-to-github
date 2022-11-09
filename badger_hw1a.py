5e9221faf89a07b193dc7ab1314f91a320633e58
ARBadger999 <116907120+ARBadger999@users.noreply.github.com>
2022-11-09 12:48:25 -0500

    SAMPLE CODE
    here is some basic code to practice with

---
+++ badger_hw1a.py
@@ -0,0 +1,15 @@
+# File: badger_hw1a.py
+#Author: Abigail Badger
+# Date:09/16/2022
+# Section:1004
+#E-mail: abigail.badger@maine.edu
+#Description:
+# This program is an algorithm for getting to know a person in my department
+#Collaboration:
+#I wrote this program alone with some assistance from COS 125 staff
+print("Hi, friend!")
+name=input("What is your name?")
+print("Hi ")
+print (name)
+print ("I am happy to meet you.")
+print ("Let's go get ice cream.")
